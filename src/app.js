81ec24ed2523b6296c9f19493f54ef7d7bf2eae3
nishant bhateja <nishant.bhateja1994@gmail.com>
2022-09-13 01:27:07 +0530

    added

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials') 
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
