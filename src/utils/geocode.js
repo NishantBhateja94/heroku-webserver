05894d5fff95bcedf781860625a58a89cc5967f8
nishant bhateja <nishant.bhateja1994@gmail.com>
2022-09-13 12:27:47 +0530

    fixed geocode error and forecast also

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,12 +1,12 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibmlzaGFudC1iaGF0ZWphIiwiYSI6ImNsN3p1MnFxMzA0eHozdm1lMWl6YnlvNWwifQ._Jud9EHYoIXzfm3jNK2SGg'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (body.features && body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
